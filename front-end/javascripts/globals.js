19fcd53cb3e028a6fafd6c17f3a09d80a8cda2fc
rachelmarin <Rachelmarinpal@gmail.com>
2021-03-04 07:58:59 -0500

    const

---
+++ front-end/javascripts/globals.js
@@ -0,0 +1,12 @@
+  
+/** Global Variables **/
+
+/** Node Getters **/
+
+const main = () => document.getElementById("main");
+const ratingInput = () => document.getElementById("rating");
+const contentInput = () => document.getElementById("content");
+const villagerInput = () => document.getElementById("villager");
+const form = () => document.getElementById("form");
+const formLink = () => document.getElementById("form-link");
+const reviewsLink = () => document.getElementById("reviews-link");
